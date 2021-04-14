19a26437a7e6190008d75b710bdd6dbd837137ab
Thanh Nhan Nguyen <mac@wifi.mshome.net>
2021-04-14 09:55:52 +0700

    my final push

--- script.js
+++ script.js
@@ -7,27 +7,27 @@
 
         main_fun: function () {
 
-             //CUSTOM LINKS SCROLLING FUNCTION 
+            //CUSTOM LINKS SCROLLING FUNCTION 
 
             $('nav a[href*=#]').click(function () {
                 if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-               && location.hostname == this.hostname) {
+                    && location.hostname == this.hostname) {
                     var $target = $(this.hash);
                     $target = $target.length && $target
-                    || $('[name=' + this.hash.slice(1) + ']');
+                        || $('[name=' + this.hash.slice(1) + ']');
                     if ($target.length) {
                         var targetOffset = $target.offset().top;
                         $('html,body')
-                        .animate({ scrollTop: targetOffset }, 800); //set scroll speed here
+                            .animate({ scrollTop: targetOffset }, 800); //set scroll speed here
                         return false;
                     }
                 }
             });
-         
 
-                //NAV SCRIPTS
+
+            //NAV SCRIPTS
             $(window).bind('scroll', function () {
-                var navHeight = $(window).height() -50;
+                var navHeight = $(window).height() - 50;
                 if ($(window).scrollTop() > navHeight) {
                     $('nav').addClass('fixed');
                 }
@@ -51,3 +51,25 @@
     });
 
 }(jQuery));
+
+
+// Instantiate the Bootstrap carousel
+$('.multi-item-carousel').carousel({
+    interval: false
+});
+
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$('.multi-item-carousel .item').each(function () {
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    if (next.next().length > 0) {
+        next.next().children(':first-child').clone().appendTo($(this));
+    } else {
+        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    }
+});
